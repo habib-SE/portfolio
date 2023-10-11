f75bd47517c3c44d23cab50c21db759c962d8444
habib-SE <hab67603@gmail.com>
2023-10-11 23:28:40 +0500

    make a work component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Navbar/>
       <Home/>
       <About/>
-      <Skills/>s
+      <Skills/>
+      <Work/>
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import Finance from '../projects/finance.PNG'
+import Drive from '../projects/drive.PNG';
+import Habib from '../projects/habib-tapha.PNG';
+import Tasty from '../projects/Tasty-food.PNG';
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Finance,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Drive,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Habib,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"HTML, CSS and JS Application",
+        image:Tasty,
+        github:"",
+        live:"",
+    },
+
+
+]
