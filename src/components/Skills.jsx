bffb164d4a4c0bc0c45a8be9caf1d025657d1c71
Habib Ur Rahman <107296557+habib-SE@users.noreply.github.com>
2023-10-16 20:14:16 +0500

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
@@ -10,9 +9,11 @@ import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
 
+// This is the skills section
 const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300 '>
+      
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
